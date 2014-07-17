4d60063d9c903d1f03c67e1e5900731ab297f432
manxisuo <manxisuo@gmail.com>
2014-07-17 23:33:09 +0800

    Signed-off-by: manxisuo <manxisuo@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,49 +1,103 @@
-var img_sel;
 var cur_index = 0;
 
-function initSelector() {
-	img_sel = $('#pic');
-}
+var postList = [];
 
 function log(msg) {
 	console.log(msg);
 }
 
-function showPost() {
-	img_sel.attr('src', 'image/' + cur_index + '.jpg');
-	if (cur_index > 0) {
-		$('.prev').attr('href', '#' + (cur_index - 1));
-	}
-	$('.next').attr('href', '#' + (cur_index + 1));
+function initPostList(callback) {
+	$.get('list.js', function(resp) {
+		postList = JSON.parse(resp);
+		callback();
+	});
 }
 
-function showPostList() {
+function showPostWithIndex(index) {
+	var post = postList[index];
 	
+	content.update('image/' + post.name + '.jpg', post.title, post.desc, post.date);
+	
+	if (index > 0) {
+		$('.prev').attr('href', '#' + postList[index - 1].name);
+	}
+	
+	if (index < postList.length - 1) {
+		$('.next').attr('href', '#' + postList[index + 1].name);
+	}
 }
+
+function show404Page() {
+	content.update('image/NotFound.jpg', 'Not Found', 'Not Found', null);
+
+	$('.prev').attr('href', '#' + postList[0].name);
+	$('.next').attr('href', '#' + postList[0].name);
+}
+
+function showPostWithName(name) {
+	for (var i = 0, len = postList.length; i < len; i++) {
+		if (postList[i].name == name) {
+			showPostWithIndex(i);
+			
+			return;
+		}
+	}
+	
+	show404Page();
+}
+
 
 // 路由
 function handleHashChange(hash) {
     if ('' == hash) {
         // showPostList();
-        showPost(0);
+        showPostWithIndex(0);
     }
     else if (hash.charAt(0) == '#') {
-		var index = hash.substring(1, hash.length);
-		cur_index = parseInt(index);
-		showPost();
+		var name = hash.substring(1, hash.length);
+		showPostWithName(name);
 	}
 	else {
 		error();
     }
 }
 
+var content = {
+	title: '',
+	desc: '',
+	date: '',
+	image: '',
+	update: function(image, title, desc, date) {
+		this.title = title ? title : '';
+		this.desc = desc ? desc : '';
+		this.date = date ? date : '';
+		this.image = image ? image : '';
+		this._reload();
+	},
+	_reload: function() {
+		$('#title').text(this.title);
+		$('#desc').text(this.desc);
+		$('#pic').attr('src', this.image);
+	}
+}
+
+function jumpto(hash) {
+	window.location.href = '#' + hash;
+}
+
 $(function() {
-	initSelector();
+	$(document).on('keypress', function(e) {
+		
+		log(e);
+	});
 	
 	// hash change event
     $(window).hashchange(function(){
         handleHashChange(location.hash);
     });
     
-    $(window).hashchange();
+    initPostList(function() {
+		 $(window).hashchange();
+	})
+   
 });
--- list.js
+++ list.js
@@ -1,9 +1,56 @@
 [
 	{
-		index: 0,
-		name: '0.jpg',
-		title: 'Under The Dome',
-		desc: 'Under The Dome',
-		date: '2014.07.08'
+		"name": "0",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.08"
+	},
+	{
+		"name": "1",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.09"
+	},
+	{
+		"name": "2",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.10"
+	},
+	{
+		"name": "3",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.11"
+	},
+	{
+		"name": "4",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.12"
+	},
+	{
+		"name": "5",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.12"
+	},
+	{
+		"name": "6",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.12"
+	},
+	{
+		"name": "7",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.12"
+	},
+	{
+		"name": "8",
+		"title": "Under The Dome",
+		"desc": "Under The Dome",
+		"date": "2014.07.12"
 	}
 ]
