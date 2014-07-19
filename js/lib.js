85f583e00d68faab5cd7c5ab21605ea1861cf30c
manxisuo <manxisuo@gmail.com>
2014-07-20 00:17:35 +0800

    重大更新：重构。OOP和解耦合。
    1. 抽象出Pager和Poster。
    2. 解耦合。main.js负责配置变换的部分，不变的部分放到lib.js。

--- js/lib.js
+++ js/lib.js
@@ -1,15 +1,30 @@
+
+function jumpto(url) {
+	window.location.href = url;
+}
+
+function setTitle(title) {
+	document.title = title + ' - All the Lost';
+}
+
+function isValidHash(hash) {
+	return !isNaN(parseInt(hash.substring(1, hash.length)));
+}
+
+// Pager ---------------------------------------------------------------------
+
 function Pager(prevEl, nextEl) {
 	this.prevEl = prevEl;
 	this.nextEl = nextEl;
 }
 
-Pager.prototype.setPrev = function(filename) {
-	this.prevEl.attr('href', '#' + filename);
+Pager.prototype.setPrev = function(index) {
+	this.prevEl.attr('href', '#' + index);
 	this.setPrevVisible(true);
 }
 
-Pager.prototype.setNext = function(filename) {
-	this.nextEl.attr('href', '#' + filename);
+Pager.prototype.setNext = function(index) {
+	this.nextEl.attr('href', '#' + index);
 	this.setNextVisible(true);
 }
 
@@ -24,9 +39,160 @@ Pager.prototype.setNextVisible = function(visible) {
 }
 
 Pager.prototype.toPrev = function() {
-	jumpto(this.prevEl.attr('href'));
+	if ('none' != this.prevEl.css('display')) {
+		jumpto(this.prevEl.attr('href'));
+	}
 }
 
 Pager.prototype.toNext = function() {
-	jumpto(this.nextEl.attr('href'));
+	if ('none' != this.nextEl.css('display')) {
+		jumpto(this.nextEl.attr('href'));
+	}
+}
+
+// Poster ---------------------------------------------------------------------
+
+function Poster(postList, pager) {
+	this.postList = postList;
+	this.pager = pager;
+}
+
+Poster.prototype.init = function(option) {
+	this.titleEl = option.titleEl;
+	this.descEl = option.descEl;
+	this.dateEl = option.dateEl;
+	this.imageEl = option.imageEl;
+	this.commentEl = option.commentEl;
+	this.popWin = option.popWin;
+	
+	this.IMAGE_DIR = option.imageDir;
+}
+
+Poster.prototype.show404Page = function() {
+	this._updatePost(this.IMAGE_DIR + 'NotFound.jpg', 'Not Found', 'Not Found', null);
+	
+	var pager = this.pager;
+	pager.setPrev(0);
+	pager.setNext(0);
+	pager.setPrevVisible(false);
+	pager.setNextVisible(false);
+}
+
+Poster.prototype.showAboutPage = function() {
+	this._updatePost(this.IMAGE_DIR + 'About.jpg', '关于', '一个涂鸦的集合', null);
+
+	var pager = this.pager;
+	pager.setPrev(0);
+	pager.setNext(0);
+	pager.setPrevVisible(false);
+	pager.setNextVisible(false);
+}
+
+
+Poster.prototype.showPost = function (index) {
+	var pager = this.pager;
+
+	if (index < 0 || index >= this.postList.length) {
+		this.show404Page();
+		
+		return;
+	}
+
+	var post = this.postList[index];
+
+	this._updatePost(getImageUrl(post), post.title, post.desc, post.date);
+	
+	if (index > 0) {
+		pager.setPrev(index - 1);
+	}
+	else {
+		pager.setPrev(0);
+		pager.setPrevVisible(false);
+	}
+
+	
+	if (index < this.postList.length - 1) {
+		pager.setNext(index + 1);
+	}
+	else {
+		pager.setNext(this.postList.length - 1);
+		pager.setNextVisible(false);
+	}
+}
+
+Poster.prototype.prev = function() {
+	this.pager.toPrev();
+}
+
+Poster.prototype.next = function() {
+	this.pager.toNext();
+}
+
+// 注册方向键按下事件
+Poster.prototype.addKeydownSupport = function() {
+	var KEY_LEFT = 37, KEY_RIGHT = 39;
+	var _this = this;
+	
+	$('body').attr('tabindex', 0).on('keydown', function(e) {
+		switch(e.which) {
+			case KEY_LEFT:
+				e.preventDefault();
+				_this.prev();
+				break;
+			case KEY_RIGHT:
+				e.preventDefault();
+				_this.next();
+		}
+	});
+}
+
+Poster.prototype._updatePost = function(imageUrl, title, desc, date) {
+	var dest_title = title ? title : '';
+	var dest_desc = desc ? desc : '';
+	var dest_imgUrl = imageUrl ? imageUrl : '';
+	
+	var titleEl = this.titleEl;
+	var descEl = this.descEl;
+	var popWin = this.popWin;
+	var imageEl = this.imageEl;
+	var _this = this;
+	
+	if (imageEl.attr('src') != dest_imgUrl) {
+		popWin.show(5000);
+		
+		this._updateImage(dest_imgUrl, function() {
+			setTitle(dest_title);
+			titleEl.text(dest_title);
+			
+			descEl.text(dest_desc);
+			
+			
+			var threadKey = imageUrl.substring(_this.IMAGE_DIR.length, imageUrl.lastIndexOf('.'));
+			_this._updateComment(threadKey, dest_title);
+			
+			popWin.hide();
+		});
+	}
+}
+
+Poster.prototype._updateImage = function(imgUrl, callback) {
+	var imageEl = this.imageEl;
+	
+	imageEl.attr('src', imgUrl);
+	
+	imageEl.on('load', function(e) {
+		if(callback) {
+			callback();
+		}
+	});
+}
+
+Poster.prototype._updateComment = function(postId, title) {
+	var dsThread = $('<div />');
+	dsThread.attr('data-thread-key', postId);
+	dsThread.attr('data-title', title);
+	dsThread.attr('data-url', location.href);
+	DUOSHUO.EmbedThread(dsThread[0]);
+	
+	this.commentEl.html(dsThread);
 }
--- js/main.js
+++ js/main.js
@@ -1,169 +1,62 @@
-var KEY_LEFT = 37, KEY_RIGHT = 39;
+var POST_SOURCE_URL = 'list.js';
 
-var postList = [];
+var IMAGE_DIR = 'image/';
 
-var popWin = null;
-
-var tipWin = null;
-
-var pager = null;
-
-function log(msg) {
-	console.log(msg);
-}
+var poster = null;
 
 function initPostList(callback) {
-	$.get('list.js', function(resp) {
-		postList = JSON.parse(resp);
-		callback();
+	$.get(POST_SOURCE_URL, function(resp) {
+		var postList = JSON.parse(resp);
+		callback(postList);
 	});
 }
 
-function showPostWithIndex(index) {
-	var post = postList[index];
-
-	updatePost('image/' + post.name + '.jpg', post.title, post.desc, post.date);
-	
-	if (index > 0) {
-		pager.setPrev(postList[index - 1].name);
-	}
-	else {
-		pager.setPrev(postList[0].name);
-		pager.setPrevVisible(false);
-	}
-	
-	if (index < postList.length - 1) {
-		pager.setNext(postList[index + 1].name);
-	}
-	else {
-		pager.setNext(postList[postList.length - 1].name);
-		pager.setNextVisible(false);
-	}
+function getImageUrl(post) {
+	return 'image/' + post.name + '.jpg';
 }
-
-function show404Page() {
-	updatePost('image/NotFound.jpg', 'Not Found', 'Not Found', null);
-
-	pager.setPrev(postList[0].name);
-	pager.setNext(postList[0].name);
-	pager.setPrevVisible(false);
-	pager.setNextVisible(false);
-}
-
-function showAboutPage() {
-	updatePost('image/About.jpg', '关于', '一个涂鸦的集合', null);
-
-	pager.setPrev(postList[0].name);
-	pager.setNext(postList[0].name);
-	pager.setPrevVisible(false);
-	pager.setNextVisible(false);
-}
-
-function showPostWithName(name) {
-	for (var i = 0, len = postList.length; i < len; i++) {
-		if (postList[i].name == name) {
-			showPostWithIndex(i);
-			
-			return;
-		}
-	}
-	
-	show404Page();
-}
-
 
 // 路由
 function handleHashChange(hash) {
     if ('' == hash) {
-        showPostWithIndex(0);
+        poster.showPost(0);
     }
 	else if ('#NotFound' == hash) {
-		show404Page();
+		poster.show404Page();
 	}
 	else if ('#About' == hash) {
-		showAboutPage();
+		poster.showAboutPage();
 	}
-    else if (hash.charAt(0) == '#') {
-		var name = hash.substring(1, hash.length);
-		showPostWithName(name);
+    else if (isValidHash(hash)) {
+		var index = parseInt(hash.substring(1, hash.length));
+		poster.showPost(index);
 	}
 	else {
-		error();
+		poster.show404Page();
     }
-}
-
-function setTitle(title) {
-	document.title = title + ' - All the Lost';
-}
-
-function updatePost(image, title, desc, date) {
-	var dest_title = title ? title : '';
-	var dest_desc = desc ? desc : '';
-	var dest_img = image ? image : '';
-	
-	if ($('#pic').attr('src') != dest_img) {
-		popWin.show(5000);
-		updateImage(dest_img, function() {
-			setTitle(dest_title);
-			$('#title').text(dest_title);
-			
-			$('#desc').text(dest_desc);
-			
-			reloadComment(image.substring('image/'.length, image.lastIndexOf('.')), dest_title);
-			popWin.hide();
-		});
-	}
-}
-
-function updateImage(dest_img, callback) {
-	$('#pic').attr('src', dest_img);
-	
-	$('#pic').on('load', function(e) {
-		if(callback) {
-			callback();
-		}
-	});
-}
-
-function reloadComment(postId, title) {
-	var dsThread = $('<div />');
-	dsThread.attr('data-thread-key', postId);
-	dsThread.attr('data-title', title);
-	dsThread.attr('data-url', location.href);
-	DUOSHUO.EmbedThread(dsThread[0]);
-	
-	$('.widget').html(dsThread);
-}
-
-function jumpto(hash) {
-	window.location.href = hash;
 }
 
 $(function() {
-	popWin = new PopWin($('#loading'));
-	tipWin = new PopWin();
-	pager = new Pager($('.prev'), $('.next'));
-
-	// 注册方向键按下事件
-	$('body').attr('tabindex', 0).on('keydown', function(e) {
-		switch(e.which) {
-			case KEY_LEFT:
-				e.preventDefault();
-				pager.toPrev();
-				break;
-			case KEY_RIGHT:
-				e.preventDefault();
-				pager.toNext();
-		}
-	});
-	
 	// hash change event
     $(window).hashchange(function(){
         handleHashChange(location.hash);
     });
     
-    initPostList(function() {
-		 $(window).hashchange();
+    initPostList(function(postList) {
+		poster = new Poster(postList, new Pager($('.prev'), $('.next')));
+		poster.init({
+			titleEl: $('#title'),
+			descEl: $('#desc'),
+			dateEl: $('#date'),
+			imageEl: $('#pic'),
+			commentEl: $('.widget'),
+			popWin: new PopWin($('#loading')),
+			imageDir: IMAGE_DIR
+		});
+		
+		poster.addKeydownSupport();
+	
+		$(window).hashchange();
 	})
    
+
 });
